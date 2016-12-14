9c80f524b12a5b8c9b5ca2f750cc1c80dcc8aaa0
Gavy Amornsirithada <gavyjay@users.noreply.github.com>
2016-12-14 00:02:49 -0500

    Update textresponse.php

--- textresponse.php
+++ textresponse.php
@@ -1,5 +1,5 @@
-$txtarray = strtolower(array('Hi'=>'Hello!',
+$txtarray = array('Hi'=>'Hello!',
 		'How are you?'=>'Fine, Thank you :)',
 		'Hey'=>'Hey! What\'s up? \n What can I do for you?',
     'Thank you'=>'You\'re wellcome!',
-));
+);
