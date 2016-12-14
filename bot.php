95ea7032236e4d7108234a73e9f1ea634a2a63bb
Gavy Amornsirithada <gavyjay@users.noreply.github.com>
2016-12-13 21:25:17 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,12 +1,30 @@
 <?php
 $access_token = 'ecSSSh4xNn+Ku1HctPBfLytO+DX+nQiugW8Nkqopalvo3c33Auaho90Uct8SA8122v3N7KW+tVPMGV1VXSGqLFqKEM4XjQUtNXAwBbIBz/sWitLy0X5xZDDHmM/q40RBY3RlOaEyxI7kAtKM1JTviQdB04t89/1O/w1cDnyilFU=';
 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ecSSSh4xNn+Ku1HctPBfLytO+DX+nQiugW8Nkqopalvo3c33Auaho90Uct8SA8122v3N7KW+tVPMGV1VXSGqLFqKEM4XjQUtNXAwBbIBz/sWitLy0X5xZDDHmM/q40RBY3RlOaEyxI7kAtKM1JTviQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b8a878b50708ee96de46ed1a3fb4b505']);
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-// Validate parsed JSON data
+
 if (!is_null($events['events'])) {
+	$replyToken = $event['replyToken'];
+	$text = $event['message']['text'];	
+	$response = $bot->replyText($replyToken, $text);
+	
+	if ($response->isSucceeded()) {
+   		echo 'Succeeded!';
+    		return;
+	}
+
+// Failed
+	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+}
+
+// Validate parsed JSON data
+/*if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -43,5 +61,5 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		}
 	}
-}
+}*/
 echo "OK";
