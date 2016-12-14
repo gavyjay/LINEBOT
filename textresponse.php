d62d10005fe475174c4fa230829188bbbf3972d5
Gavy Amornsirithada <gavyjay@users.noreply.github.com>
2016-12-14 00:26:41 -0500

    Update textresponse.php

--- textresponse.php
+++ textresponse.php
@@ -1,7 +1,9 @@
 <?php
 return array('Hi'=>'Hello!',
 		'How are you?'=>'Fine, Thank you :)',
-		'Hey'=>'Hey! What\'s up?<br> What can I do for you?',
-    'Thank you'=>'You\'re wellcome!'
+		'Hey'=>'Hey! What\'s up? What can I do for you?',
+    		'Thank you'=>'You\'re wellcome!',
+	        'เคลือบแก้ว'=>'เคลือบโปรแกรมไหนดีคะ?',
+	        'โปรแกรมเคลือบแก้ว'=>'https://www.facebook.com/albatroscarcare',
 );
 ?>
