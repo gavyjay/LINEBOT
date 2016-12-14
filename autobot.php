2ddebbc44f3cad8f5617d9005d7d1e96d3c5e1d6
Gavy Amornsirithada <gavyjay@users.noreply.github.com>
2016-12-13 21:16:33 -0500

    Create autobot.php

---
+++ autobot.php
@@ -0,0 +1,3 @@
+<?php
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ecSSSh4xNn+Ku1HctPBfLytO+DX+nQiugW8Nkqopalvo3c33Auaho90Uct8SA8122v3N7KW+tVPMGV1VXSGqLFqKEM4XjQUtNXAwBbIBz/sWitLy0X5xZDDHmM/q40RBY3RlOaEyxI7kAtKM1JTviQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b8a878b50708ee96de46ed1a3fb4b505']);
