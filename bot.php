b47e053c59966511e1ff804b7c6665fb2d2313d0
Gavy Amornsirithada <gavyjay@users.noreply.github.com>
2016-12-13 21:31:42 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'ecSSSh4xNn+Ku1HctPBfLytO+DX+nQiugW8Nkqopalvo3c33Auaho90Uct8SA8122v3N7KW+tVPMGV1VXSGqLFqKEM4XjQUtNXAwBbIBz/sWitLy0X5xZDDHmM/q40RBY3RlOaEyxI7kAtKM1JTviQdB04t89/1O/w1cDnyilFU=';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ecSSSh4xNn+Ku1HctPBfLytO+DX+nQiugW8Nkqopalvo3c33Auaho90Uct8SA8122v3N7KW+tVPMGV1VXSGqLFqKEM4XjQUtNXAwBbIBz/sWitLy0X5xZDDHmM/q40RBY3RlOaEyxI7kAtKM1JTviQdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b8a878b50708ee96de46ed1a3fb4b505']);
 
 // Get POST body content
@@ -9,7 +9,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 
-if (!is_null($events['events'])) {
+/*if (!is_null($events['events'])) {
 	$replyToken = $event['replyToken'];
 	$text = $event['message']['text'];	
 	$response = $bot->replyText($replyToken, $text);
@@ -21,10 +21,10 @@ if (!is_null($events['events'])) {
 
 // Failed
 	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-}
+}*/
 
 // Validate parsed JSON data
-/*if (!is_null($events['events'])) {
+if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -42,6 +42,7 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
+			//$response = $bot->replyText($replyToken, $text);
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
@@ -61,5 +62,5 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		}
 	}
-}*/
+}
 echo "OK";
